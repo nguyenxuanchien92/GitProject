b94f02bd47c7090516f509c757857be7eea743ec
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-10-07 09:35:03 +0700

    hiển thị đầu ra dùng JS

--- javaScript/alert.js
+++ javaScript/alert.js
@@ -1,9 +1,21 @@
 let evtClick = document.getElementById("btClickMe");
+let divMess = document.getElementById("message");
 
 function displayMess() {
-    alert("Xin Chào");
+    // alert("Xin Chào");
+    //Bài 2
+    // divMess.innerText = "Xin chào"
+    //Bài 3
+    document.write("Xin chào");
+    //bài 4
+    console.log("Xin chào");
 }
 
-evtClick.addEventListener('click',displayMess);
+if(evtClick){
+    evtClick.addEventListener('click',displayMess);
+}else {
+    alert("Không bắt được sự kiện");
+}
+
 // evClick.addEventListener("click",alert("Xin chào"));
 
